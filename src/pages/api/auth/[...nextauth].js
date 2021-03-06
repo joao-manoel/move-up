7f7f42f71e244c631ca5965e68502a2956c38f6e
joao-manoel <joaomanoel.ms.br@gmail.com>
2021-03-06 02:30:23 -0400

    remove provider github

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -8,11 +8,7 @@ export default NextAuth({
       clientId: "430469541025-96le74j0snfdggookq9boj112324ia9q.apps.googleusercontent.com",
       clientSecret: "svfpk06VcBpI0sGVWmWOXHFa",
       authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
-    }),
-    Providers.GitHub({
-      clientId: "ca3fae23b480ba749440",
-      clientSecret: "945cedcb723b125e54bc8fc4b45c62fcd8085c8a"
-    }),
+    })
   ],
 
   session: {
