4c1612d76d4b028c39c767393f208940e02b0402
joao-manoel <joaomanoel.ms.br@gmail.com>
2021-03-06 02:34:35 -0400

    1 minute

--- src/contexts/CountContext.tsx
+++ src/contexts/CountContext.tsx
@@ -22,7 +22,7 @@ export function CountProvider({children}: CountProviderProps){
 
   const { startNewChallenge } = useContext(ChallengesContext);
   
-  const [time, setTime] = useState(0.1 * 60);
+  const [time, setTime] = useState(1 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
